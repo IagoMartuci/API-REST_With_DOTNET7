08e0d26c57ffdf4b61494571a6f38a701de8241f
IagoMartuci <martuciiago1992@gmail.com>
2023-09-03 22:15:07 -0300

    Pessoas BD

---
+++ 03_API-REST_With_DOTNET7_Pessoas_BD/API-REST_With_DOTNET7/Program.cs
@@ -0,0 +1,35 @@
+using API_REST_With_DOTNET7.Model.Context;
+using API_REST_With_DOTNET7.Services.Implementations;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// Injeção de dependencia da interface
+builder.Services.AddScoped<IPessoaService, PessoaServiceImplementation>();
+
+// https://stackoverflow.com/questions/72670847/using-pomelo-entityframeworkcore-mysql-to-connect-to-mysql-with-net-6-on-mac
+var connString = builder.Configuration["MySQLConn:MySQLConnString"];
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 33));
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connString, serverVersion));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
